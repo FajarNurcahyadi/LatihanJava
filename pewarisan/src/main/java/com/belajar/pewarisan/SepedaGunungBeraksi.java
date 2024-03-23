0841fc9118fe358b71f8c1819180e15bb9ef691f
FajarNurcahyadi <fajar.nurcahyadi515@gmail.com>
2024-03-23 19:04:14 +0700

    latihan pewarisan

---
+++ pewarisan/src/main/java/com/belajar/pewarisan/SepedaGunungBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.belajar.pewarisan;
+
+/**
+ *
+ * @author fajar
+ */
+public class SepedaGunungBeraksi {
+    
+    public static void main(String[] args) {
+        SepedaGunung sepedaGunung = new SepedaGunung();
+    
+        sepedaGunung.tambahKecepatan(10);
+        sepedaGunung.ubahGir(2);
+        sepedaGunung.setSadel(20);
+    }
+}
