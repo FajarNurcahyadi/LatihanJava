6d820088577bc8ff35a3875bebc4bfb445cfec4a
FajarNurcahyadi <fajar.nurcahyadi515@gmail.com>
2024-03-23 19:06:28 +0700

    create project polymerphisme

---
+++ polymorphism/polymorphism/src/main/java/com/belajar/polymorphism/Polymorphism.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.belajar.polymorphism;
+
+/**
+ *
+ * @author fajar
+ */
+public class Polymorphism {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
