e76df46376d08e9fc22be80d454e621ee975f579
FajarNurcahyadi <164319984+FajarNurcahyadi@users.noreply.github.com>
2024-03-24 23:01:08 +0700

    Add files via upload

---
+++ operator aritmatika/operatorAritmatika/src/main/java/operatoraritmatika/OperatorAritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package operatoraritmatika;
+
+/**
+ *
+ * @author fajar
+ */
+public class OperatorAritmatika {
+
+    public static void main(String[] args) {
+    int i = 9;
+    int j = 4;
+    
+    System.out.println ("Nilai Variable :   ");
+    System.out.println ("I =   " + i);
+    System.out.println ("J =    " + j);
+    
+    System.out.println ("Penjumlahan :   ");
+    System.out.println ("I + J =   "+ (i + j));
+    
+    System.out.println ("Pengurangan :   ");
+    System.out.println ("I - J =   "+ (i - j));
+    
+    System.out.println ("Perkalian :   ");
+    System.out.println ("I * J =   "+ (i * j));
+    
+    System.out.println ("Pembagian :   ");
+    System.out.println ("I / J =   "+ (i / j));
+     
+    System.out.println ("Modulus :   ");
+    System.out.println ("I % J =   "+ (i % j));
+     
+     
+     
+}
+}
---
+++ operator aritmatika/operatorAritmatika/src/main/java/operatoraritmatika/OptLogikaLanjutan.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operatoraritmatika;
+
+/**
+ *
+ * @author fajar
+ */
+public class OptLogikaLanjutan {
+    public static void main(String[] args) {
+    
+        System.out.println("Operator AMD");
+        System.out.println("True & True = " +(true & true));
+        System.out.println("True & False = "  +(true & false));
+        System.out.println("False & True = "  +(false & true)); 
+        System.out.println("False & False = " +(false & false));
+        System.out.println("Operasi OR");
+        System.out.println("True || True = "  +(true  || true));
+        System.out.println("True || False = "  +(true || false));
+        System.out.println("False || True = "  +(false || true)); 
+        System.out.println("False || False = " +(false || false));
+        System.out.println("\nOperator NOT");
+        System.out.println("!True =  " +(!true));
+        System.out.println("!False = "+(!false));
+    }
+}
