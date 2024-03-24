76ec8c0d4191572ab4e02404f4b9ead7a70eb1fc
FajarNurcahyadi <164319984+FajarNurcahyadi@users.noreply.github.com>
2024-03-24 22:58:56 +0700

    Add files via upload

--- polymorphism/polymorphism/src/main/java/com/belajar/polymorphism/Polymorphism.java
+++ polymorphism/polymorphism/src/main/java/com/belajar/polymorphism/Polymorphism.java
@@ -8,9 +8,45 @@ package com.belajar.polymorphism;
  *
  * @author fajar
  */
-public class Polymorphism {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    class hewan {
+            public void suara(){
+            System.out.println("Hewan Bersuara :");   
+        }
     }
-}
+    class kuda extends hewan {
+            public void suara(){
+            System.out.println("Kuda Mengikik...");
+        }
+     }
+    class kucing extends hewan {
+            public void suara(){
+            System.out.println("Kucing Mengeong...");
+        }
+      }
+    class ayam extends hewan {
+            public void suara(){
+            System.out.println("Ayam Berkokok...");
+        }
+    }
+    public class Polymorphism {
+            public static void main (String[] args){
+            hewan HEWAN = new hewan();
+            kuda KUDA = new kuda();
+            kucing KUCING = new kucing();
+            ayam AYAM = new ayam();
+        
+            HEWAN.suara();
+        
+            HEWAN = KUDA;
+            HEWAN.suara();
+        
+            HEWAN = KUCING;
+            HEWAN.suara();
+        
+            HEWAN = AYAM;
+            HEWAN.suara();
+            }
+        
+       }
