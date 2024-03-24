74a8201502d0c3135cd102070f164708a977f4dd
FajarNurcahyadi <164319984+FajarNurcahyadi@users.noreply.github.com>
2024-03-24 23:19:01 +0700

    Add files via upload

---
+++ belajarjava/src/main/java/belajarjava/belajarjava/Belajarjava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package belajarjava.belajarjava;
+
+/**
+ *
+ * @author fajar
+ */
+class Belajar
+{
+public String x = "Pintar";
+public String y = "Java";
+    
+}
