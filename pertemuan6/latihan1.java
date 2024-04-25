eceb4b32ed7eb555c1bba3d744fe5d8497c74141
FajarNurcahyadi <164319984+FajarNurcahyadi@users.noreply.github.com>
2024-04-25 15:27:59 +0700

    Add files via upload

---
+++ pertemuan6/latihan1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan6;
+
+/**
+ *
+ * @author fajar
+ */
+import javax.swing.*;
+
+
+/**
+ *
+ * @author ezran
+ */
+class latihan1 {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        
+        myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+    }
+}
